e555057ce932bf7ddb933b9a78021bd982520657
YeongG <yeonggil223@gmail.com>
2021-03-23 23:43:10 +0900

    [CREATE] asset index.ts

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+export { default as cameraIcon } from "./cameraIcon.svg";
+export { default as uploadIcon } from "./uploadIcon.svg";
+export { default as loadingIcon } from "./loadingIcon.svg";
+export { default as selectIcon } from "./selectIcon.svg";
+export { default as realTimeIcon } from "./realtimeIcon.svg";
+export { default as pictureIcon } from "./pictureIcon.svg";
