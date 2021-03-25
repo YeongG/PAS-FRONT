2f663b694c5015a95d0e6a1bdc89091e34ed36ba
YeongG <yeonggil223@gmail.com>
2021-03-25 22:30:28 +0900

    [MODIFY] asset index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,3 +4,6 @@ export { default as loadingIcon } from "./loadingIcon.svg";
 export { default as selectIcon } from "./selectIcon.svg";
 export { default as realTimeIcon } from "./realtimeIcon.svg";
 export { default as pictureIcon } from "./pictureIcon.svg";
+export { default as galleryIcon } from "./galleryIcon.svg";
+export { default as recordIcon } from "./recordIcon.svg";
+export { default as recodingIcon } from "./recodingIcon.svg";
