5972ca48c3f04e6b21548602a4a6849ac5d29b51
YeongG <yeonggil223@gmail.com>
2021-03-25 22:31:10 +0900

    [MODIFY] realtime page/style

--- src/pages/Realtime/RealtimePage.tsx
+++ src/pages/Realtime/RealtimePage.tsx
@@ -1,11 +1,23 @@
-import React, { FC, useEffect, useState } from "react";
+import React, {
+  FC,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+  MouseEvent,
+} from "react";
 import * as S from "./style";
 import { useDispatch } from "react-redux";
 import { setSliderStep } from "../../modules/action/slider";
+import { galleryIcon, recodingIcon, recordIcon } from "../../assets";
 
 const RealtimePage: FC = () => {
   const dispatch = useDispatch();
   const [imgSrcArr, setImgSrcArr] = useState<string[]>([]);
+  const [historyIsOpen, setHistoryIsOpen] = useState<boolean>(false);
+  const [isRecoding, setIsRecoding] = useState<boolean>(false);
+  const videoRef = useRef<HTMLVideoElement>();
+  const canvasRef = useRef<HTMLCanvasElement>();
 
   useEffect(() => {
     dispatch(setSliderStep("realtime"));
@@ -15,40 +27,68 @@ const RealtimePage: FC = () => {
         video: true,
       })
       .then((stream) => {
-        const videoElement: HTMLVideoElement = document.getElementById(
-          "video"
-        ) as HTMLVideoElement;
-        videoElement.srcObject = stream;
-        videoElement.play();
-
-        const canvas: HTMLCanvasElement = document.getElementById(
-          "canvas"
-        ) as HTMLCanvasElement;
-
-        setInterval(() => {
-          canvas.width = videoElement.videoWidth;
-          canvas.height = videoElement.videoHeight;
-
-          canvas
-            .getContext("2d")
-            .drawImage(videoElement, 0, 0, canvas.width, canvas.height);
-          const img: string = canvas.toDataURL("image/png");
-          setImgSrcArr((prev) => [...prev, img]);
-        }, 2000);
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
       });
   }, []);
 
+  useEffect(() => {
+    if (isRecoding) {
+      let intervalId: NodeJS.Timeout = setInterval(() => {
+        canvasRef.current.width = videoRef.current.videoWidth;
+        canvasRef.current.height = videoRef.current.videoHeight;
+
+        canvasRef.current
+          .getContext("2d")
+          .drawImage(
+            videoRef.current,
+            0,
+            0,
+            canvasRef.current.width,
+            canvasRef.current.height
+          );
+        const img: string = canvasRef.current.toDataURL("image/png");
+        setImgSrcArr((prev) => [...prev, img]);
+      }, 3000);
+
+      return () => clearInterval(intervalId);
+    }
+  }, [isRecoding]);
+
+  const toggleHistoryisOpen = useCallback(() => {
+    setHistoryIsOpen((prev) => !prev);
+  }, []);
+
+  const toggleisRecoding = useCallback(() => {
+    setIsRecoding((prev) => !prev);
+  }, []);
+
+  const backgroundRef = useCallback((e: MouseEvent<HTMLDivElement>) => {
+    if (e.currentTarget === e.target) setHistoryIsOpen(false);
+  }, []);
   return (
-    <div>
-      <video id="video" style={{ width: "640px", height: "400px" }} />
-      <canvas id="canvas" style={{ display: "none" }} />
-      {imgSrcArr.length}
-      <S.ImgWrap>
-        {imgSrcArr.map((imgSrc) => (
-          <S.ImgItem key={imgSrc} src={imgSrc} />
-        ))}
-      </S.ImgWrap>
-    </div>
+    <S.Container>
+      <S.RealTimeCameraWrap>
+        <S.RecordStatusShowIconWrap onClick={toggleisRecoding}>
+          <img src={isRecoding ? recodingIcon : recordIcon} />
+        </S.RecordStatusShowIconWrap>
+
+        <S.RealTimeVideo ref={videoRef} id="video" />
+        <S.RealTimeCanvas ref={canvasRef} id="canvas" />
+        <S.GalleryWrap>
+          <S.GalleryImg onClick={toggleHistoryisOpen} src={galleryIcon} />
+        </S.GalleryWrap>
+      </S.RealTimeCameraWrap>
+      <S.ImgBackground onClick={backgroundRef} isActive={historyIsOpen}>
+        <S.ImgWrap>
+          {imgSrcArr.map((imgSrc, i) => (
+            <S.ImgItemDiv>
+              <S.ImgItem key={imgSrc} src={imgSrc} />
+            </S.ImgItemDiv>
+          ))}
+        </S.ImgWrap>
+      </S.ImgBackground>
+    </S.Container>
   );
 };
 
--- src/pages/Realtime/style.ts
+++ src/pages/Realtime/style.ts
@@ -1,8 +1,85 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+export const Container = styled.div``;
+export const RecordStatusShowIconWrap = styled.div`
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  border: 3px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 50px;
+    height: 50px;
+  }
+`;
+export const RealTimeCameraWrap = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const ImgItem = styled.img`
-  width: 400px;
+  width: 100%;
+  box-sizing: border-box;
+  object-fit: cover;
+
   height: 400px;
 `;
 
-export const ImgWrap = styled.div``;
+export const ImgItemDiv = styled.div`
+  flex: 1 1 30%;
+  max-width: 500px;
+  height: 420px;
+  margin: 0 10px;
+  box-sizing: border-box;
+`;
+
+export const RealTimeVideo = styled.video`
+  width: 1000px;
+  height: 600px;
+  margin-top: 20px;
+`;
+
+export const RealTimeCanvas = styled.canvas`
+  display: none;
+`;
+
+export const ImgBackground = styled.div<{ isActive: boolean }>`
+  top: 0;
+  left: 0;
+  position: fixed;
+  background-color: #eee;
+  z-index: 2;
+  width: 100%;
+  height: 100vh;
+  overflow-y: auto;
+
+  ${(props) =>
+    !props.isActive &&
+    css`
+      display: none;
+    `}
+`;
+export const ImgWrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 1600px;
+  margin: 0 auto;
+  margin-top: 200px;
+`;
+export const GalleryWrap = styled.div`
+  position: fixed;
+  right: 20px;
+  top: 20px;
+`;
+
+export const GalleryImg = styled.img`
+  width: 60px;
+  height: 60px;
+`;
