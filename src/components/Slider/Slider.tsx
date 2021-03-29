69d01d1da02bf157d18bfcaf57ac484d649f19ed
YeongG <yeonggil223@gmail.com>
2021-03-29 23:33:36 +0900

    [MODIFY] analysis 페이지 로딩시 슬라이더 변경

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { useSelector } from "react-redux";
-import { realTimeIcon, selectIcon, uploadIcon } from "../../assets";
+import { checkIcon, realTimeIcon, selectIcon, uploadIcon } from "../../assets";
 import { Store } from "../../modules/reducer";
 import * as S from "./style";
 
@@ -11,7 +11,7 @@ const Slider: FC = () => {
       <S.IconImg isActive={step === "select"} src={selectIcon} />
       <S.IconImg isActive={step === "upload"} src={uploadIcon} />
       <S.IconImg isActive={step === "realtime"} src={realTimeIcon} />
-      <S.IconImg isActive={step === "c"} src={uploadIcon} />
+      <S.IconImg isActive={step === "analysis"} src={checkIcon} />
       <S.IconImg isActive={step === "d"} src={uploadIcon} />
       <S.IconImg isActive={step === "e"} src={uploadIcon} />
       <S.IconImg isActive={step === "f"} src={uploadIcon} />
