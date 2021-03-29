778649ed297dcc671b473d99bcdf20d809f323f0
YeongG <yeonggil223@gmail.com>
2021-03-29 23:37:56 +0900

    [MODIFY] redux reducer index.ts

--- src/modules/reducer/index.ts
+++ src/modules/reducer/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import analysisReducer from "./analysis";
 import sliderReducer from "./slider";
 
 const rootReducer = combineReducers({
   slider: sliderReducer,
+  analysis: analysisReducer,
 });
 
 export default rootReducer;
