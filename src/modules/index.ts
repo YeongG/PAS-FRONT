22bd1b6ccb40d1bcc3eae87f343d690074f316cc
YeongG <yeonggil223@gmail.com>
2021-03-29 23:37:45 +0900

    [MODIFY] redux-saga 사용시 history 쓸수 있게 변경

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,7 +1,20 @@
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { createBrowserHistory } from "history";
+import createReduxSaga from "redux-saga";
 import rootReducer from "./reducer";
+import rootSaga from "./saga";
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()));
+export const history = createBrowserHistory();
+const sagaInstance = createReduxSaga({
+  context: {
+    history,
+  },
+});
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaInstance))
+);
+sagaInstance.run(rootSaga);
 
 export default store;
