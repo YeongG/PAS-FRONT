5a628c6c6a4d65b8434bdb7ede9a1344075cf215
YeongG <yeonggil223@gmail.com>
2021-03-29 23:38:27 +0900

    [CREATE] page index.ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as IndexPage } from "./Index/IndexPage";
 export { default as UploadPage } from "./Upload/UploadPage";
 export { default as RealtimePage } from "./Realtime/RealtimePage";
+export { default as AnalysisPage } from "./Analysis/AnalysisPage";
