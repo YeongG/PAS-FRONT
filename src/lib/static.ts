e1c7141a0e7f293e62bed0b7011a6240884fdca8
YeongG <yeonggil223@gmail.com>
2021-04-06 23:51:24 +0900

    [MODIFY] static 데이터 정의

--- src/lib/static.ts
+++ src/lib/static.ts
@@ -1,1 +1,29 @@
+import { AnalysisNavItem } from "../components/AnalysisNav/AnalysisNav";
+
 export const allowedExtensions: string[] = ["jpg", "png", "jpeg"];
+export const analysisItems: AnalysisNavItem[] = [
+  {
+    name: "Objects",
+    value: "localizedObject",
+  },
+  {
+    name: "Labels",
+    value: "label",
+  },
+  {
+    name: "Logos",
+    value: "logo",
+  },
+  {
+    name: "Text",
+    value: "text",
+  },
+  {
+    name: "Properties",
+    value: "imagePropertie",
+  },
+  {
+    name: "Safe Search",
+    value: "safeSearch",
+  },
+];
