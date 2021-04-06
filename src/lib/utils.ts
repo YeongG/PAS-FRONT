065f340ac315f5c56750c4149f82ad6e26c042c0
YeongG <yeonggil223@gmail.com>
2021-04-06 23:51:43 +0900

    [MODIFY] getCanvasBorderSize 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,31 @@
+import { Vertice } from "./payloads/analysis";
 import { allowedExtensions } from "./static";
 
 export const isAllowedFile = (extension: string): boolean => {
   return allowedExtensions.includes(extension.toLowerCase());
+};
+
+export const getCanvasBorderSize = (
+  vertices: Vertice[],
+  size: { width: number; height: number }
+) => {
+  const { width, height } = size;
+
+  const [vertice1, _, vertice3] = vertices;
+
+  const { x: x1, y: y1 } = vertice1;
+  const { x: x2, y: y2 } = vertice3;
+
+  const canvasX1 = x1 * width;
+  const canvasX2 = x2 * width;
+
+  const canvasY1 = y1 * height;
+  const canvasY2 = y2 * height;
+
+  return {
+    x1: canvasX1,
+    y1: canvasY1,
+    width: canvasX2 - canvasX1,
+    height: canvasY2 - canvasY1,
+  };
 };
