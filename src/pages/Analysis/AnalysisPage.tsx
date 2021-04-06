b20c7207e3aa507eb0a65f37ab7c6b5256e34d92
YeongG <yeonggil223@gmail.com>
2021-04-06 23:52:01 +0900

    [MODIFY] Analysis page/style

--- src/pages/Analysis/AnalysisPage.tsx
+++ src/pages/Analysis/AnalysisPage.tsx
@@ -1,13 +1,94 @@
-import React, { FC, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, {
+  FC,
+  ReactEventHandler,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { AnalysisMenu, AnalysisNav } from "../../components";
+import { AnalysisNavItem } from "../../components/AnalysisNav/AnalysisNav";
+import { AnalysisType } from "../../lib/payloads/analysis";
+import { analysisItems } from "../../lib/static";
+import { getCanvasBorderSize } from "../../lib/utils";
 import { setSliderStep } from "../../modules/action/slider";
+import { Store } from "../../modules/reducer";
+import * as S from "./style";
 
 const AnalysisPage: FC = () => {
+  const history = useHistory();
   const dispatch = useDispatch();
+  const [nowType, setNowType] = useState<AnalysisType>("localizedObject");
+  const { originalImgSrc, analysis } = useSelector(
+    (store: Store) => store.analysis
+  );
+
+  const canvasRef = useRef<HTMLCanvasElement>();
+  const imgRef = useRef<HTMLImageElement>();
+
   useEffect(() => {
     dispatch(setSliderStep("analysis"));
   }, []);
-  return <div></div>;
+
+  // useEffect(() => {
+  //   if (!originalImgSrc) {
+  //     toast.error("잘못된 접근 입니다.");
+  //     history.push("/");
+  //   }
+  // }, [originalImgSrc]);
+
+  const navChangeHandler = useCallback((data: AnalysisNavItem) => {
+    setNowType(data.value);
+  }, []);
+
+  const imgLoadHandler = useCallback(
+    (e) => {
+      const { width, height } = e.currentTarget;
+
+      canvasRef.current.width = width;
+      canvasRef.current.height = height;
+
+      const ctx = canvasRef.current.getContext("2d");
+      ctx.strokeStyle = "rgb(118, 255, 3)";
+      ctx.lineWidth = 8;
+
+      const {
+        height: drawHeight,
+        width: drawWidth,
+        x1,
+        y1,
+      } = getCanvasBorderSize(
+        analysis.localizedObjectAnnotations[0].boundingPoly.normalizedVertices,
+        { width, height }
+      );
+
+      ctx.strokeRect(x1, y1, drawWidth, drawHeight);
+    },
+    [analysis]
+  );
+
+  return (
+    <S.Container>
+      <div>
+        <S.MenuNav>
+          <AnalysisNav datas={analysisItems} onChange={navChangeHandler} />
+        </S.MenuNav>
+        <S.RelativeDiv>
+          <S.OriginalImg
+            ref={imgRef}
+            onLoad={imgLoadHandler}
+            src={originalImgSrc}
+          ></S.OriginalImg>
+          <S.Canvas ref={canvasRef} />
+          <S.MenuWrap>
+            <AnalysisMenu {...analysis} nowType={nowType} />
+          </S.MenuWrap>
+        </S.RelativeDiv>
+      </div>
+    </S.Container>
+  );
 };
 
 export default AnalysisPage;
--- src/pages/Analysis/style.ts
+++ src/pages/Analysis/style.ts
@@ -1,5 +1,38 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  align-items: center;
+  flex-direction: column;
+`;
+export const Canvas = styled.canvas`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+export const RelativeDiv = styled.div`
+  position: relative;
+  display: flex;
+`;
+export const OriginalImg = styled.img`
+  max-width: 1000px;
+  max-height: 700px;
+  height: auto;
+  object-fit: cover;
+`;
+
+export const MenuWrap = styled.div`
+  width: 350px;
+  margin-left: 30px;
+  min-height: 500px;
+  max-height: 700px;
+`;
+
+export const MenuNav = styled.div`
+  min-width: 700px;
+  margin-bottom: 10px;
 `;
