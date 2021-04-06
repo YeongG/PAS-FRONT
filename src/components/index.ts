54a67513f20fb5d0cd382ee59054e7a0bd72bbda
YeongG <yeonggil223@gmail.com>
2021-04-06 23:52:54 +0900

    [MODIFY] component index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export { default as Slider } from "./Slider/Slider";
+export { default as AnalysisMenu } from "./AnalysisMenu/AnalysisMenu";
+export { default as PersentBar } from "./PersentBar/PersentBar";
+export { default as AnalysisNav } from "./AnalysisNav/AnalysisNav";
