f18423a263f78c245ee995ea301e0bf63f32eac1
YeongG <yeonggil223@gmail.com>
2021-04-11 23:46:24 +0900

    [MODIFY] 분석 메뉴 기능 추가

--- src/components/AnalysisMenu/AnalysisMenu.tsx
+++ src/components/AnalysisMenu/AnalysisMenu.tsx
@@ -1,6 +1,10 @@
 import React, { FC, useMemo } from "react";
-import { AnalysisType, GoogleVisionRes } from "../../lib/payloads/analysis";
-import PersentBar from "../PersentBar/PersentBar";
+import { ProgressBlock, PersentBar, TextBlock, ColorAnalysis } from "..";
+import {
+  AnalysisSafeValue,
+  AnalysisType,
+  GoogleVisionRes,
+} from "../../lib/payloads/analysis";
 import * as S from "./styles";
 
 interface Props extends GoogleVisionRes {
@@ -28,13 +32,11 @@ const AnalysisMenu: FC<Props> = ({
         return <div></div>;
       }
       case "imagePropertie": {
-        return localizedObjectAnnotations.map(({ name, score }) => (
-          <PersentBar
-            key={name}
-            persent={Math.round(score * 100)}
-            name={name}
+        return (
+          <ColorAnalysis
+            colors={imagePropertiesAnnotation.dominantColors.colors}
           />
-        ));
+        );
       }
       case "label": {
         return labelAnnotations.map(({ score, description }) => (
@@ -46,16 +48,64 @@ const AnalysisMenu: FC<Props> = ({
         ));
       }
       case "localizedObject": {
-        return <div></div>;
+        return localizedObjectAnnotations.map(({ name, score }) => (
+          <PersentBar
+            key={name}
+            persent={Math.round(score * 100)}
+            name={name}
+          />
+        ));
       }
       case "logo": {
-        return <div></div>;
+        return logoAnnotations.map(({ description, score }) => (
+          <PersentBar
+            key={description}
+            persent={Math.round(score * 100)}
+            name={description}
+          />
+        ));
       }
       case "safeSearch": {
-        return <div></div>;
+        const { adult, medical, racy, spoof, violence } = safeSearchAnnotation;
+        return (
+          <>
+            <ProgressBlock
+              max={5}
+              name={"성인"}
+              value={adult}
+              progress={AnalysisSafeValue[adult]}
+            />
+            <ProgressBlock
+              max={5}
+              name={"spoof"}
+              value={spoof}
+              progress={AnalysisSafeValue[spoof]}
+            />
+            <ProgressBlock
+              max={5}
+              name={"의료"}
+              value={medical}
+              progress={AnalysisSafeValue[medical]}
+            />
+            <ProgressBlock
+              max={5}
+              name={"violence"}
+              value={violence}
+              progress={AnalysisSafeValue[violence]}
+            />
+            <ProgressBlock
+              max={5}
+              name={"racy"}
+              value={racy}
+              progress={AnalysisSafeValue[racy]}
+            />
+          </>
+        );
       }
       case "text": {
-        return <div></div>;
+        return textAnnotations.map(({ description }) => (
+          <TextBlock content={description} />
+        ));
       }
     }
   }, [nowType]);
