51ab45a69fc8eae3de5c2671eee796883a93a018
YeongG <yeonggil223@gmail.com>
2021-04-11 23:47:16 +0900

    [MODIFY] component index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export { default as Slider } from "./Slider/Slider";
 export { default as AnalysisMenu } from "./AnalysisMenu/AnalysisMenu";
 export { default as PersentBar } from "./PersentBar/PersentBar";
 export { default as AnalysisNav } from "./AnalysisNav/AnalysisNav";
+export { default as ProgressBlock } from "./ProgressBlock/ProgressBlock";
+export { default as TextBlock } from "./TextBlock/TextBlock";
+export { default as ColorAnalysis } from "./ColorAnalysis/ColorAnalysis";
