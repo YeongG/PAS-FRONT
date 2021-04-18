b33f5115a48d1f7bea8d225bf9fbf664f4dac106
YeongG <yeonggil223@gmail.com>
2021-04-18 21:07:08 +0900

    [MODIFY] lib/payload auth 타입 추가

--- src/lib/payloads/auth/index.ts
+++ src/lib/payloads/auth/index.ts
@@ -3,6 +3,12 @@ export interface AuthReq {
   password: string;
 }
 
+export interface RegisterReq {
+  id: string;
+  password: string;
+  name: string;
+}
+
 export interface AuthRes {
   accessToken: string;
 }
