7a5c277c2dffe4fc599e9b1a2ce7d00fcf68cbc3
YeongG <yeonggil223@gmail.com>
2021-04-22 22:40:41 +0900

    [MODIFY] 페이지 로딩시 로그인 여부 체크

--- src/pages/Modal/ModalPage.tsx
+++ src/pages/Modal/ModalPage.tsx
@@ -1,13 +1,26 @@
-import React, { FC } from "react";
-import { useSelector } from "react-redux";
+import React, { FC, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Store } from "../../modules/reducer";
 import * as S from "./styles";
 import { modalConstant } from "../../lib/payloads/modal";
 import LoginModal from "./Login/LoginModal";
 import RegisterModal from "./Register/RegisterModal";
+import * as authApi from "../../lib/api/auth";
+import { setModalType } from "../../modules/action/loginModal";
+import { useHistory } from "react-router";
 
 const Modal: FC = () => {
+  const dispatch = useDispatch();
   const { isOpen, type } = useSelector((store: Store) => store.loginModal);
+
+  useEffect(() => {
+    authApi
+      .reqCheckAuth()
+      .then(() => {
+        dispatch(setModalType(modalConstant.SUCCESS));
+      })
+      .catch((err) => {});
+  }, []);
 
   return (
     isOpen && (
