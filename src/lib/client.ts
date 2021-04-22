e1e0cb75c30a06d13b0271c1e0a770901fa5669f
YeongG <yeonggil223@gmail.com>
2021-04-22 22:39:50 +0900

    [MODIFY] lib/client.ts header추가

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,10 +1,20 @@
 import axios, { AxiosInstance } from "axios";
 
-const BASE_URL = "http://localhost";
+const BASE_URL = "http://localhost" as const;
+const ACCESS_TOKEN = "ACCESS_TOKEN" as const;
+
+export const authConstant = {
+  ACCESS_TOKEN,
+};
 
 const apiDefault = (): AxiosInstance => {
+  const accessToken: string = localStorage.getItem(ACCESS_TOKEN);
+
   const axiosInstance = axios.create({
     baseURL: BASE_URL,
+    headers: {
+      Authorization: accessToken,
+    },
   });
 
   return axiosInstance;
