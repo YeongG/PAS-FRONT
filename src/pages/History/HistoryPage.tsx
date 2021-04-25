75e0f11bda17f524b579ab0201e15340309930eb
YeongG <yeonggil223@gmail.com>
2021-04-25 23:40:21 +0900

    [CREATE] History page withLogin 테스트

---
+++ src/pages/History/HistoryPage.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import { withLogin } from "../../lib/hoc";
+
+const HistoryPage: FC = () => {
+  return <div>역사</div>;
+};
+
+export default withLogin(HistoryPage);
