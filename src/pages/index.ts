d9a930ae484edb7b6f626da4e4592ffcddc61439
YeongG <yeonggil223@gmail.com>
2021-04-25 23:41:30 +0900

    [MODIFY] page index.ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export { default as UploadPage } from "./Upload/UploadPage";
 export { default as RealtimePage } from "./Realtime/RealtimePage";
 export { default as AnalysisPage } from "./Analysis/AnalysisPage";
 export { default as ModalPage } from "./Modal/ModalPage";
+export { default as HistoryPage } from "./History/HistoryPage";
